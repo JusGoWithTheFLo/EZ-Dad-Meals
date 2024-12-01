03e62e81b0f8a7c4965631e8531d5458853b74f6
JusGoWithTheFLo <107014024+JusGoWithTheFLo@users.noreply.github.com>
2024-12-01 13:53:48 -0500

    Removed blank spaces in stylesheet and created a recipe class to store recipes for future reference

---
+++ script.js
@@ -0,0 +1,33 @@
+const recipes = [
+    {
+        name: "BBQ Chicken Pizza",
+        difficulty: "Easy",
+        time: "45min",
+        servingSize: 4,
+        image: "",
+        ingredients: [
+            "1 loaf of french bread",
+            "3/4 cup bbq sauce, divided",
+            "2 cups mozzarella cheese, shredded",
+            "1 cup chicken, cooked and shredded",
+            "1/4 cup red onion, chopped",
+            "1-2 Tbsp cilantro, chopped"
+        ],
+        procedure: [
+            "Preheat your oven to 400 degrees.",
+            "Cut your french bread load in half (hot dog style).",
+            "Spread bbq sauce evenly over the top of each french bread load (leave 2 Tbsp extra for drizzling at the end).",
+            "Sprinkle about 1-1/2 cups of shredded mozzarella cheese evenly over the top of each half.",
+            "Spread chicken and red onion evenly over both loaves.",
+            "Top with remaining cheese.",
+            "Bake in oven for about 12-17 minutes, or until cheese is melted and starts to turn golden.",
+            "Remove pizza from oven and let cool for about 2 minutes.",
+            "Top pizza wth cilantro and remaining bbq sauce."
+        ],
+        notes: [
+            "Serve with a side salad.",
+            "Use rotisserie chicken to make this even qucker!",
+            "Keep onions off kids portions if they don't like it."
+        ]
+    }
+]
